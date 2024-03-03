7cb907d30bc4518b09db072286ac73fd84a4d075
sb <sb@gmail.com>
2024-03-03 09:24:02 -0600

    Eslint configurado y package-lock arreglado

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  extends: [
+    "plugin:astro/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect", // Detecta automáticamente la versión de React
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+        ecmaVersion: 2020,
+      },
+      env: {
+        "astro/astro": true,
+        es2020: true,
+      },
+    },
+  ],
+  rules: {
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "always-multiline",
+      },
+    ],
+  },
+};
