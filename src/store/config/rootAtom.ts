fb1721381946420db599af99e2ff6a2934cec807
sb <sb@gmail.com>
2024-03-07 13:09:40 -0600

    load route info in FStore

--- src/store/config/rootAtom.ts
+++ src/store/config/rootAtom.ts
@@ -1,10 +1,12 @@
 import { deepMap } from "nanostores";
 import { APP_INFO_INIT as appInfo } from "../appInfo";
 import { SCREEN_INFO_INIT as screenInfo } from "../screenInfo";
+import { ROUTE_INFO_INIT as routeInfo } from "../routeInfo";
 
 export const INITIAL_REDUCERS = {
   appInfo,
   screenInfo,
+  routeInfo,
 };
 
 export type IFStore = typeof INITIAL_REDUCERS;
---
+++ src/store/routeInfo.ts
@@ -0,0 +1,5 @@
+import { URL } from "url";
+
+export type RouteInfoState = Partial<URL> | undefined;
+
+export const ROUTE_INFO_INIT: RouteInfoState = undefined;
