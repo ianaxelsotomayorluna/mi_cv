0d4b93c4fd68b644cfb45a64cb11c94c2b5532a0
sb <sb@gmail.com>
2024-06-24 15:10:33 -0600

    Imagen de typescript fixed

--- src/config/skillsInfo.tsx
+++ src/config/skillsInfo.tsx
@@ -7,7 +7,7 @@ import ChatGPT from "src/assets/images/Skills/ChatGPT.png?jsx";
 import MidJourney from "src/assets/images/Skills/MidJourney.png?jsx";
 import CSS from "src/assets/images/Skills/CSS.png?jsx";
 import HTML from "src/assets/images/Skills/HTML.png?jsx";
-import TypeScript from "src/assets/images/Skills/TypeScript.png?jsx";
+import TypeScript from "src/assets/images/Skills/typeScript.png?jsx";
 import UI from "src/assets/images/Skills/UI.png?jsx";
 import Figma from "src/assets/images/Skills/Figma.png?jsx";
 import StableDiffusion from "src/assets/images/Skills/StableDiffusion.png?jsx";
